3083a69e33ad86f1fabc571283a18893db85acd0
John Jardin <john@agilite.io>
2024-05-23 09:27:46 +0200

    Upgraded module to TypeScript

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import { performance, PerformanceObserver } from 'perf_hooks'
+
+const timing = {}
+
+export const timeStart = (key) => {
+  if (!key) return null
+  timing[key] = performance.now()
+  return true
+}
+
+export const timeEnd = (key, format = 'ms') => {
+  if (!key) return null
+
+  const start = timing[key]
+  if (!start) return null
+
+  let formatValue = 1000
+
+  switch (format.toLowerCase()) {
+    case 'ns':
+      formatValue = 1
+      break
+  }
+
+  const end = performance.now()
+  const result = (end - start) / formatValue
+
+  console.log(`${key}: ${result} ${format}`)
+  delete timing[key]
+
+  return true
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,21 +1,21 @@
-import { expect } from "chai";
-import { timeStart, timeEnd } from "../index.js";
+import { expect } from 'chai'
+import { timeStart, timeEnd } from '../index.js'
 
-describe("nano-performance", function () {
-  it("should correctly measure execution time", function (done) {
-    this.timeout(3000); // increase timeout for this test
+describe('nano-performance', function () {
+  it('should correctly measure execution time', function (done) {
+    this.timeout(3000) // increase timeout for this test
 
-    const key = "test";
-    timeStart(key);
+    const key = 'test'
+    timeStart(key)
 
     setTimeout(() => {
-      timeEnd(key, "ms");
+      timeEnd(key, 'ms')
 
       // We can't assert the exact time because it depends on the system,
       // but we can check if it's approximately correct.
-      expect(timing[key]).to.be.closeTo(2000, 100);
+      expect(timing[key]).to.be.closeTo(2000, 100)
 
-      done();
-    }, 2000);
-  });
-});
+      done()
+    }, 2000)
+  })
+})
